c8ef30e17ad8cf67ec667fc448f4b17bae0a75e0
Fritz-Stevenson <fritzstevenson@gmail.com>
2021-03-29 22:37:12 -0700

    After a long slog, I added the function that adds characters after a point threshold has been reached. Until tomorrow.

--- profile_functions.py
+++ profile_functions.py
@@ -1,17 +1,44 @@
-import os, csv
+import os, csv, random
 import dictionary_repository as dr
 from datetime import datetime
-from pathlib import Path
+import hsk_repository as hr
 
 
-class ProfileManipulator:
-    def __init__(self, profile_name, x):
+class ProfileObject:
+    def __init__(self, profile_name):
+        self.list = []
         self.profile_name = profile_name
         if self.check_if_profile_exists() == False:
             self.make_profile_folder()
             self.create_profile()
         else:
             self.fetch_csv_to_dict()
+        self.HSK_Level = int(dr.profile_dictionary['HSK_Level'])
+        self.HSk_points = [dr.HSK_dictionary['HSK1 Points'],
+                           dr.HSK_dictionary['HSK2 Points'],
+                           dr.HSK_dictionary['HSK3 Points'],
+                           dr.HSK_dictionary['HSK4 Points'],
+                           dr.HSK_dictionary['HSK5 Points'],
+                           dr.HSK_dictionary['HSK6 Points']
+                           ]
+        self.HSK_thresholds = [dr.HSK_dictionary['HSK1 Threshold'],
+                               dr.HSK_dictionary['HSK2 Threshold'],
+                               dr.HSK_dictionary['HSK3 Threshold'],
+                               dr.HSK_dictionary['HSK4 Threshold'],
+                               dr.HSK_dictionary['HSK5 Threshold'],
+                               dr.HSK_dictionary['HSK6 Threshold']
+                                ]
+
+    def check_to_add_characters(self):
+        for x in range(len(self.HSk_points)):
+            HSK_level = x+1
+            if self.HSk_points[x] > self.HSK_thresholds[x]:
+                string = f'hsk{HSK_level}_vocab'
+                local_HSK_list = getattr(hr, string)
+                for i in range(int(len(local_HSK_list) * .08)):
+                    char_list_difference = list(set(local_HSK_list) - set(self.list))
+                    if len(char_list_difference) != 0:
+                        self.list.append(char_list_difference[random.randint(0, (len(char_list_difference)) - 1)])
 
     def check_if_profile_exists(self):
         return bool(os.path.exists(f'player_profiles\\{self.profile_name}'))
@@ -28,6 +55,7 @@ class ProfileManipulator:
         profile_dict['titles'] = ['Initiate']
         profile_dict['Join Date'] = datetime.strftime(datetime.now(), "%m/%d/%Y")
         profile_dict['Profile age'] = None
+        profile_dict['HSK_Level'] = 0
         HSK_dict = {}
         HSK_dict['HSK Level'] = 0
         HSK_dict['HSK1 Points'] = 0
@@ -35,21 +63,46 @@ class ProfileManipulator:
         HSK_dict['HSK3 Points'] = 0
         HSK_dict['HSK4 Points'] = 0
         HSK_dict['HSK5 Points'] = 0
-        self.profile_dict.append(profile_dict)
+        HSK_dict['HSK6 Points'] = 0
+        HSK_dict['HSK1 Threshold'] = 5
+        HSK_dict['HSK2 Threshold'] = 5
+        HSK_dict['HSK3 Threshold'] = 5
+        HSK_dict['HSK4 Threshold'] = 5
+        HSK_dict['HSK5 Threshold'] = 5
+        HSK_dict['HSK6 Threshold'] = 5
+        dr.profile_dictionary = profile_dict
         dr.HSK_dictionary = HSK_dict
         dr.profile_dictionary = profile_dict
+
+    def test_HSK_level(self, char):
+        if char in ''.join(hr.hsk1_vocab) and char not in ''.join(hr.hsk2_vocab):
+            return 1
+        elif char in ''.join(hr.hsk2_vocab) and char not in ''.join(hr.hsk3_vocab):
+            return 2
+        elif char in ''.join(hr.hsk3_vocab) and char not in ''.join(hr.hsk4_vocab):
+            return 3
+        elif char in ''.join(hr.hsk4_vocab) and char not in ''.join(hr.hsk5_vocab):
+            return 4
+        elif char in ''.join(hr.hsk5_vocab) and char not in ''.join(hr.hsk6_vocab):
+            return 5
+        elif char in ''.join(hr.hsk6_vocab):
+            return 6
+        else:
+            return 0
+
+    def check_if_characters_in_repository(self):
         for i in self.list:
-            if not any(d['Name'] == i for d in self.dict):
+            if not any(d['Name'] == i for d in dr.character_dictionary):
                 char_dict = {}
                 char_dict['Name'] = i
                 char_dict['Accuracy'] = 0
                 char_dict['HSK_Level'] = self.test_HSK_level(i)
                 char_dict['Weight'] = 0
                 dr.character_dictionary.append(char_dict)
+        # No implementation of this yet
 
     def create_run_dict(self):
         pass
-
 
     def fetch_csv_to_dict(self):
         old_dir = os.getcwd()
@@ -69,8 +122,7 @@ class ProfileManipulator:
 
         os.chdir(old_dir)
 
-
-    def save_dictionaries(self):
+    def save_files(self):
         old_dir = os.getcwd()
         os.chdir(f'player_profiles\\{self.profile_name}')
 
@@ -88,5 +140,22 @@ class ProfileManipulator:
             for i in dr.character_dictionary:
                 writer.writerow(i)
         os.chdir(old_dir)
-v = ProfileManipulator('Fritz', 'x')
-v.save_dictionaries()
+
+    def calculate_HSK_points(self):
+        (dr.HSK_dictionary['HSK1 Points'],dr.HSK_dictionary['HSK2 Points'],dr.HSK_dictionary['HSK3 Points'],
+        dr.HSK_dictionary['HSK4 Points'],dr.HSK_dictionary['HSK5 Points'],dr.HSK_dictionary['HSK6 Points']) =0
+        for i in dr.character_dictionary:
+            if i['HSK_Level'] == 1:
+                dr.HSK_dictionary['HSK1 Points'] += float(i['Accuracy'])/len(hr.hsk1_vocab)
+            elif i['HSK_Level'] == 2:
+                dr.HSK_dictionary['HSK2 Points'] += float(i['Accuracy'])/len(hr.hsk2_new)
+            elif i['HSK_Level'] == 3:
+                dr.HSK_dictionary['HSK3 Points'] += float(i['Accuracy'])/len(hr.hsk3_new)
+            elif i['HSK_Level'] == 4:
+                dr.HSK_dictionary['HSK4 Points'] += float(i['Accuracy'])/len(hr.hsk4_new)
+            elif i['HSK_Level'] == 5:
+                dr.HSK_dictionary['HSK5 Points'] += float(i['Accuracy'])/len(hr.hsk5_new)
+            elif i['HSK_Level'] == 6:
+                dr.HSK_dictionary['HSK6 Points'] += float(i['Accuracy'])/len(hr.hsk6_new)
+v = ProfileObject('Fritz')
+v.save_files()
--- quiz_functions.py
+++ quiz_functions.py
@@ -11,29 +11,12 @@ analyzer = ChineseAnalyzer()
 class QuizList:
     def __init__(self, li, profile, question_count):
         self.profile = profile
+        self.pf_object = pf.ProfileObject(profile)
         self.list = hr.hsk1_vocab #self.decide_hsk()
         self.list_length = len(li)
-        self.pf_object = pf.ProfileManipulator()
+        self.analyzer =ChineseAnalyzer()
         for i in range(question_count):
             self.serve_question()
-
-
-    def test_HSK_level(self, char):
-        if char in ''.join(hr.hsk1_vocab) and char not in ''.join(hr.hsk2_vocab):
-            return 1
-        elif char in ''.join(hr.hsk2_vocab) and char not in ''.join(hr.hsk3_vocab):
-            return 2
-        elif char in ''.join(hr.hsk3_vocab) and char not in ''.join(hr.hsk4_vocab):
-            return 3
-        elif char in ''.join(hr.hsk4_vocab) and char not in ''.join(hr.hsk5_vocab):
-            return 4
-        elif char in ''.join(hr.hsk5_vocab) and char not in ''.join(hr.hsk6_vocab):
-            return 5
-        elif char in ''.join(hr.hsk6_vocab):
-            return 6
-        else:
-            return 0
-
 
     def serve_question(self):
         char = self.list[random.randint(0,len(self.list))]
@@ -71,24 +54,28 @@ class QuizList:
         print(f'Entering a special round! Your character is {token}\n')
         answer= input('Enter a definition, the correct pinyin and tone, or ') # create another option for the answers
         self.assign_calculated_adjustments(token, answer_bool)
-        self.character_list_adjustments()
+        self.pf_object.check_to_add_characters()
         print(token, pinyin, definition)
+
     def serve_question_easy(self):
         pass
+
     def approach_roll(self):
         approach = random.randint(0,3)
         return approach
+
     def create_word_list(self):
         pass
+
     def check_for_quit(self, user_input):
         if user_input.upper() != 'QUIT':
             return True
         else:
-            pass
-            #save dictionaries to csv
-            #quit program
+            pf.ProfileObject.save_files() # pf is a placeholder in this code
+
     def log_progress(self):
-        self.pf_object.save_dictionaries()
+        self.pf_object.save_files()
+
     def assign_calculated_adjustments(self, character, value):
         eq = lambda x, y: x + (1 - x) * .25 if y == True else x - (x / 4)
 
@@ -97,29 +84,12 @@ class QuizList:
         character_dict['Accuracy'] = eq(previous_accuracy, value)
         adjustment_value = character_dict['Accuracy']-previous_accuracy
         self.adjust_HSK_Points(character, adjustment_value)
-    def character_list_adjustments(self):
-        pass
+
+
     def adjust_HSK_Points(self, char, adjustment):
-        level = self.test_HSK_level(char)
+        level = self.pf_object.test_HSK_level(char)
         dr.HSK_dictionary[f'HSK{level} Points'] += float(adjustment)
 
-
-    def calculate_HSK_points(self):
-        dr.HSK_dictionary['HSK1 Points'],dr.HSK_dictionary['HSK2 Points'],dr.HSK_dictionary['HSK3 Points'],
-        dr.HSK_dictionary['HSK4 Points'],dr.HSK_dictionary['HSK5 Points'],dr.HSK_dictionary['HSK6 Points'] =0
-        for i in dr.character_dictionary:
-            if i['HSK_Level'] == 1:
-                dr.HSK_dictionary['HSK1 Points'] += float(i['Accuracy'])/len(hr.hsk1_vocab)
-            elif i['HSK_Level'] == 2:
-                dr.HSK_dictionary['HSK2 Points'] += float(i['Accuracy'])/len(hr.hsk2_new)
-            elif i['HSK_Level'] == 3:
-                dr.HSK_dictionary['HSK3 Points'] += float(i['Accuracy'])/len(hr.hsk3_new)
-            elif i['HSK_Level'] == 4:
-                dr.HSK_dictionary['HSK4 Points'] += float(i['Accuracy'])/len(hr.hsk4_new)
-            elif i['HSK_Level'] == 5:
-                dr.HSK_dictionary['HSK5 Points'] += float(i['Accuracy'])/len(hr.hsk5_new)
-            elif i['HSK_Level'] == 6:
-                dr.HSK_dictionary['HSK6 Points'] += float(i['Accuracy'])/len(hr.hsk6_new)
 
 
 
